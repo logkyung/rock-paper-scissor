f304c3fe3440d36b033d110f0280593c7bf868bf
logkyung <aebong54@gmail.com>
2023-07-19 22:54:51 +0900

    feat: choosing card to play

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+import { useState } from "react";
+import Button from "./Button";
+import HandIcon from "./HandIcon";
 import HandButton from "./HandButton";
+import { compareHand, generateRandomHand } from './utils';
+
+const INITIAL_VALUE = 'rock';
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return '승리';
+  if (comparison < 0) return '패배';
+  return '무승부';
+}
 
 function App() {
-  const handleClick = (value) => console.log(value);
+  const [ hand, setHand ] = useState(INITIAL_VALUE);
+  const [ otherHand, setOtherHand ] = useState(INITIAL_VALUE);
+
+  const handleButtonClick = (nextHand) => {
+    const nextOtherHand = generateRandomHand();
+    setHand(nextHand);
+    setOtherHand(nextOtherHand);
+  }
+  const handleClearClick = () => {
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+  }
+
   return (
     <div>
-      <HandButton value="rock" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
-      <HandButton value="scissor" onClick={handleClick} />
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <HandButton value="rock" onClick={handleButtonClick} />
+        <HandButton value="scissor" onClick={handleButtonClick} />
+        <HandButton value="paper" onClick={handleButtonClick} />
+      </div>
     </div>
   );
 }
